a684663adc577ec85c5e1e37a9a63f32a1593723
SerhiyZar <serhiy.zarichanskiy@gmail.comm>
2023-05-19 18:59:12 +0300

    Edit MainPaige & change Header 19.05 - Ser

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
+import './../style/header.scss'
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './Logo';
+import WalletAndLang from './WalletAndLang';
+
+
 const Header = () => {
     return (
-        <div>
-            <Link to='/'>Головна</Link>
-            <Link to='/about-us'>Про нас</Link>
-            <Link to='/service'>Послуга</Link>
-            <Link to='/faq'>FAQ</Link>
-            <Link to='/contacts'>Контакти</Link>
-        </div>
+        <header className='header'>
+            <div className="container">
+                <div className="header_row">
+                    <Logo/>
+                    <div className='links'>
+                        <Link to='/'>Головна</Link>
+                        <Link to='/about-us'>Про нас</Link>
+                        <Link to='/service'>Послуга</Link>
+                        <Link to='/faq'>FAQ</Link>
+                        <Link to='/contacts'>Контакти</Link>
+                    </div>
+                    <WalletAndLang/>
+                </div>
+
+            </div>
+        </header>
     );
 };
 
-export default Header;
+export default Header; 
