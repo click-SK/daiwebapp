f86dbd0f8efe59b8dfa375509c35195f6c818de9
click-SK <sashakushnir4991@gmail.com>
2023-06-03 12:14:49 +0300

    add style anim main page

--- src/components/main_page/AboutUs.jsx
+++ src/components/main_page/AboutUs.jsx
@@ -16,7 +16,10 @@ const AboutUs = () => {
                         <div className="about_info_subtitle">Він працює 24/7/365, ніколи не втомлюється і допомагає заробляти гроші на інвестуванні. <p className='adaptive_text'>Платформа для токенізації бізнесів, бізнес-процесів, будь-яких фінансових послуг, активів і пасивів.</p></div>
                     <div className="about_info_pictures">
                         <img className='img_about' src='/img/aboutus_image.png' alt="image" />
-                        <img className='logo' src='/img/composition.svg' alt="logo" />
+                        <div className='logo'>
+                            <img className='logo_img' src='/img/logo_vertical.svg' alt="logo" />
+                            <img className='logo_img2' src='/img/background_animation2.svg' alt="logo" />
+                        </div>
                     </div>
                     </div>
                 </div>
--- src/components/main_page/Service.jsx
+++ src/components/main_page/Service.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useWindowScroll } from 'react-use';
 import '../../style/service.scss'
 
 
 
 const Service = () => {
+
+  const coinOneRef = useRef(null);
+  const coinTwoRef = useRef(null);
+  const [coinOneInView, setCoinOneInView] = useState(false);
+  const [coinTwoInView, setCoinTwoInView] = useState(false);
+  const { y: scrollY } = useWindowScroll();
+
+  useEffect(() => {
+    const coinOneElement = coinOneRef.current;
+    const coinTwoElement = coinTwoRef.current;
+    if (coinOneElement) {
+      const { top, bottom } = coinOneElement.getBoundingClientRect();
+      setCoinOneInView(top < window.innerHeight && bottom >= 0);
+    }
+    if (coinTwoElement) {
+      const { top, bottom } = coinTwoElement.getBoundingClientRect();
+      setCoinTwoInView(top < window.innerHeight && bottom >= 0);
+    }
+  }, [scrollY]);
 
     return (
         <div className='service'>
@@ -23,8 +43,14 @@ const Service = () => {
                        </div>
                     </div>
                     <div className="service_info_pictures">
-                        <img className='coin_one' src='/img/coin1.svg' alt="Coin img" />
-                        <img className='coin_two' src='/img/coin2.svg' alt="Coin img" />
+                        <img 
+                        ref={coinOneRef}
+                        className={`coin_one ${coinOneInView ? 'coin_anim' : ''}`} 
+                        src='/img/coin1.svg' alt="Coin img" />
+                        <img
+                        ref={coinTwoRef}
+                        className={`coin_two ${coinTwoInView ? 'coin_anim2' : ''}`}  
+                        src='/img/coin2.svg' alt="Coin img" />
                         <div className="main_small_animation">
                             <img src="./img/main_small_bg-anim.svg" alt="animation" />
                         </div>
@@ -33,9 +59,9 @@ const Service = () => {
             </div>
             <div className="service_info_extra">
                 <div className="info_block">
-                    <span>D.A.I.Wo</span> бере на себе важкий процес управління фінансами та інвестиціями</div> 
-                <div className="info_block"><span>Штучний інтелект</span> самостійно інвестує та розподіляє кошти.</div>
-                <div className="info_block">Прозорість та безпека забезпечена <span>смарт-конктрактами</span> </div>
+                  <p> <span>  D.A.I.Wo</span> бере на себе важкий процес управління фінансами та інвестиціями </p></div> 
+                <div className="info_block"> <p><span>Штучний інтелект</span> самостійно інвестує та розподіляє кошти. </p> </div>
+                <div className="info_block"> <p>Прозорість та безпека забезпечена <span>смарт-конктрактами</span> </p>  </div>
             </div>
             <div className="know_more">
                 <button className='btn_know_more'>Дізнатись більше</button>
