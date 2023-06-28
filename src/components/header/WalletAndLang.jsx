36454a0c7798253db041e1fc00656cc8e878330a
click-SK <sashakushnir4991@gmail.com>
2023-06-28 12:25:13 +0300

    Revert "added minimal web3 functionality and created layout for modal window"
    This reverts commit 89e99d76394ed390f8600db653e69655690eda8f.

--- src/components/header/WalletAndLang.jsx
+++ src/components/header/WalletAndLang.jsx
@@ -1,37 +1,35 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 import '../../style/wallet&lang.scss'
-import { useNavigate } from 'react-router-dom';
+
 
 
 const WalletAndLang = () => {
     const [lang, setLang] = useState("УКР")
     const [isOpen, setIsOpen] = useState(false)
 
-    const navigate = useNavigate();
-
     const hendlerChangeLang = (value) => {
         setLang(value);
-    };
+      };
 
-    return (
+    return ( 
         <div className="wallet_lang">
-            <button className='secondary' onClick={() => navigate("/wallet")}>Підключити гаманець</button>
-            <div
-                onClick={() => setIsOpen(!isOpen)}
+            <button className='secondary'>Підключити гаманець</button>
+                <div 
+                onClick={()=>setIsOpen(!isOpen)}
                 className="glob_lang">
-                <img src='/icons/icons_language.svg' alt="glob" />
-                <p className='lang_select'>{lang} <img className="" src="/icons/icon_arrow-purple.svg" alt="icon"></img></p>
-                {isOpen &&
-                    <div className='lang_option'>
-                        <p onClick={(e) => hendlerChangeLang('ENG')}>ENG</p>
-                        <p onClick={(e) => hendlerChangeLang('УКР')}>УКР</p>
-                        <p onClick={(e) => hendlerChangeLang('РУС')}>РУС</p>
-                    </div>
-                }
-            </div>
-
+                    <img src='/icons/icons_language.svg' alt="glob" />
+                    <p className='lang_select'>{lang} <img class="" src="/icons/icon_arrow-purple.svg" alt="icon"></img></p>
+                    {isOpen &&
+                        <div className='lang_option'>
+                            <p onClick={(e)=>hendlerChangeLang('ENG')}>ENG</p> 
+                            <p onClick={(e)=>hendlerChangeLang('УКР')}>УКР</p> 
+                            <p onClick={(e)=>hendlerChangeLang('РУС')}>РУС</p> 
+                        </div>
+                    }
+                </div>
+        
         </div>
-    );
+     );
 }
-
+ 
 export default WalletAndLang;
